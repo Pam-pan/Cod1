f4f0c49a7dcc25affabffb7049d4e6ee8059145e
Pam-pan <pamelukitrukis2006@gmail.com>
2025-11-29 18:42:33 -0500

    Codigo fuente Cod1.c

---
+++ Cod1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+const int v0=5;
+const int v1=2;
+int resultado; 
+resultado=v1*v0;
+
+/*el %d sirve para imprimir un numero entero y con decimal base 10, aparte /n sirve para que el siguiente Código se reproduzca en la siguiente Linea*/
+printf("el resultado es: %d\n", resultado);
+
+//ahora el siguiente texto se reprducira en la siguiente linea//
+printf("Fin del codigo");
+
+return 0;
+}
